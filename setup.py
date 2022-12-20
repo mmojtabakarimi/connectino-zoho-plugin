1dbfcf953416d884d4d166f3e7d28cc6f2363262
mkarimi <mojtaba.karimi@solve-x.net>
2022-12-21 00:13:07 +0330

    update 3

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2017-2019 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
 
 from setuptools import find_packages
 from setuptools import setup
 
 setup(
     name='wazo_zoho',
-    version='1.0',
-    description='Connectino zoho plugin',
-    author='Mojtaba Karimi',
-    author_email='mmojtabakarimi@yahoo.com',
+    version='0.1',
+    description='Wazo ZOHO',
+
+    author='Wazo Authors',
+    author_email='dev@wazo.io',
+
+    url='http://wazo.io',
+
     packages=find_packages(),
-    url='https://www.foo-bar.com',
     include_package_data=True,
     package_data={
         'wazo_zoho': ['*/api.yml'],
     },
-
     entry_points={
         'wazo_auth.external_auth': [
-            'zoho = wazo_zoho.plugin:Plugin'
-        ]
+            'zoho = wazo_zoho.auth.plugin:Plugin',
+        ],
     }
 )
